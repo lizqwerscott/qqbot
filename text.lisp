b5a2be24ec78dcbf6952c9688ab405e5ef997135
lizqwerscott <lizqwerscott@163.com>
2021-09-05 10:53:57 +0800

    add miyan

--- text.lisp
+++ text.lisp
@@ -1,5 +1,21 @@
 (in-package :qqbot.text)
 
+(defparameter *mi-yan* nil)
+
+(defun load-miyan ()
+  (load-json-file (merge-pathnames "data/名言.json"
+                                   (get-source-dir))))
+
+(setf *mi-yan* (load-miyan))
+
+(defun random-lst (lst)
+  (elt lst (random-int-r (- (length lst) 1))))
+
+(defun miyanlist ()
+  (mapcar #'(lambda (x)
+              (car x))
+          *mi-yan*))
+
 (defun get-random-text (command)
   (web-get "api.vvhan.com" (format nil "api/~A" command)))
 
@@ -30,5 +46,37 @@
                  (dolist (line (bullet-fly-load))
                    (sleep 1)
                    (send-text (target-id sender) line))))
+
+(add-command "名言"
+             #'(lambda (sender args)
+                 (let ((target (target-id sender)))
+                   (if (or (args-type args (list #'numberp))
+                           (args-type args))
+                       (dotimes (i (if (car args) (parse-integer (car args)) 1))
+                         (sleep 1)
+                         (let ((miyans (random-lst *mi-yan*)))
+                           (let ((miyan (random-lst (cdr miyans))))
+                             (if (listp miyan)
+                                 (send-text-lst target miyan)
+                                 (send-text target miyan)))))
+                       (send-text target "参数错误, 例子:陈睿 名言 2 或者 陈睿 名言")))))
+
+(add-command "名言人物"
+             #'(lambda (sender args)
+                 (send-text-lst (target-id sender) (miyanlist))))
+
+(add-command "人物名言"
+             #'(lambda (sender args)
+                 (let ((target (target-id sender)))
+                   (if (args-type args (list #'symbolp))
+                       (let ((key (assoc-value *mi-yan* (car args))))
+                         (if key
+                             (dolist (i key)
+                               (sleep 1)
+                               (if (listp i)
+                                   (send-text-lst target i
+                                                  (send-text target i))))
+                             (send-text target "没有找到这个人呢!")))
+                       (send-text target "参数错误, 例子:陈睿 人物名言 缘之空")))))
 
 (in-package :cl-user)
