2e9d3669823cf7d027552df127adb871e443f8cc
lizqwerscott <lizqwerscott@163.com>
2022-10-05 23:26:07 +0800

    change class address

--- src/class-schedule.lisp
+++ src/class-schedule.lisp
@@ -7,7 +7,7 @@
 (in-package :qqbot.class-schedule)
 
 (defparameter +person-class+ nil)
-(defparameter +remote-address+ "8.219.190.193:8089")
+(defparameter +remote-address+ "124.222.100.66:8089")
 
 (defun load-class-person (path)
   (load-json-file path))
